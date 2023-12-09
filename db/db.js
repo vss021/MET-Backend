603fa521a31cc33b85bd61d4047a06d4675d9cce
Vijay Singh Saud <108565750+vss021@users.noreply.github.com>
2023-12-09 16:07:13 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect("mongodb+srv://saudvijaysingh:DJhzv0kb3o6Npa5L@cluster0.beegeiv.mongodb.net/?retryWrites=true&w=majority")
+        await mongoose.connect("mongodb+srv://saudvijaysingh:Wraz8ElEg4FKKZOH@cluster0.5qcxnfb.mongodb.net/?retryWrites=true&w=majority")
         console.log('Db Connected')
     } catch (error) {
         console.log('DB Connection Error');
